c3b2781f31d4d0760be0190767a4ebfd3a9d3ecf
August Pollard <128096377+Ljpeg@users.noreply.github.com>
2025-09-17 01:51:59 +0000

    refactor to dynamic controller route action view

--- app/controllers/rps_controller.rb
+++ app/controllers/rps_controller.rb
@@ -1,27 +1,13 @@
 # inherits ApplicationController
 
 class RpsController < ApplicationController
-  RPS = ["rock", "paper", "scissors"]
 
-  def rock
-    @them = RPS.sample
-    # use ApplicationController render method
-    # can render json, plain txt, etc
-    # use template to use .erb file 
-    # create the below folder and file in views
-    render({ :template => "game_templates/play_rock"})
-  end 
+  def dynamic
+    @rps = ["rock", "paper", "scissors"]
+    @them = @rps.sample
+    @move = params.fetch("move")
 
-  def paper
-    @them = RPS.sample
-
-    render({:template => "game_templates/play_paper"})
-  end
-
-  def scissors
-    @them = RPS.sample
-
-    render({:template => "game_templates/play_scissors"})
+    render({:template => "game_templates/play_move"})
   end 
 
   def rules
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,6 @@
 Rails.application.routes.draw do
-  # Entry point to the app
-  # all paths that users are allowed to visit
 
-  # method with argument name of the path
-  # agrument with hash literally with two keys
-  # :controller => "" (class), :action => "" (method)
-  # class is defined in app/controllers folder , method defined in class
-  # controller folder and class needs to match the name 
-
-  get("/rock", { :controller => "rps", :action => "rock"})
-
-  get("/paper", { :controller => "rps", :action => "paper"})
-
-  get("/scissors", { :controller => "rps", :action => "scissors"})
+  get("/:move", { :controller => "rps", :action => "dynamic"})
 
   get("/", { :controller => "rps", :action => "rules"})
   
